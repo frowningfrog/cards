7f48401b38ee0952099f61663b18807e907f1de6
frowningfrog <natalizedstolpinator@hotmail.com>
2025-12-06 01:56:13 -0700

    stuff

--- bj.js
+++ bj.js
@@ -3,11 +3,25 @@ function start() {
     canvas.width = window.innerWidth;   canvas.height = window.innerHeight;
     ctx = canvas.getContext("2d");
     ctx.imageSmoothingEnabled = false;  // not fuzzy yay
-    const resize = canvas.width/360;
+    const resize = canvas.height/360;
     ctx.scale(resize, resize);      // scales to window nicely
+
+    back = new Image();
+    back.src = 'res/back.png';
+    load(back);
 
     game();
 }
+
 function game() {
-    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(back, canvas.width/5, 50, 62.5, 90.75);
+
+    requestAnimationFrame(game)
 }
+
+function load(elem){        //      make        //
+    elem.onload = () => {   //      sure        //
+        draw(elem);         //      things      //
+    }                       //      load!!!     //
+}                           //      -—————-     //
