2c9e3720674d646d97a7227ad9774981e634ef72
frowningfrog <natalizedstolpinator@hotmail.com>
2025-12-12 20:53:28 -0700

    stuff

--- bj.js
+++ bj.js
@@ -4,20 +4,23 @@ function start() {
     canvas.height = window.innerHeight;
     ctx = canvas.getContext("2d");
     ctx.imageSmoothingEnabled = false;  // not fuzzy yay
-    const resize = canvas.height/100;
+    const resize = 25;
     ctx.scale(resize, resize);
 
     game();
 }
 
 function game() {
+    const rat = 1.452;
+    const w = canvas.width/100;
+    const h = w*rat;
     const dhand = [
         acespades,
         queenspades
     ];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     var x = w;
-    ctx.drawImage(back, 0, canvas.height/125, w, h);
+    ctx.drawImage(back, 0, 0, w, h);
     /*dhand.forEach(dcard => {
         ctx.drawImage(dcard, x, canvas.height/125, w, h);
         x+=w;
@@ -25,7 +28,3 @@ function game() {
 
     requestAnimationFrame(game)
 }
-
-const rat = 1.452;
-const w = (canvas.width/6 >= 50) ? 16 : canvas.width/15;
-const h = w*rat;
