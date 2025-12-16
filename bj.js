83860e2de7e6d0da3020c54e35dc0703ee6fec1e
frowningfrog <natalizedstolpinator@hotmail.com>
2025-12-15 18:48:55 -0700

    space

--- bj.js
+++ bj.js
@@ -14,7 +14,7 @@ function game() {
     const midx = canvas.width/60;
     const midy = canvas.height/60;
     const rat = 1.452;
-    const w = (canvas.width > 600 && canvas.height > 1000) ? canvas.width/100 : 5;
+    const w = (canvas.width > 600 && canvas.height > 1000) ? canvas.width/100 : 4;
     const h = w*rat;
     const dhand = [
         acespades,
@@ -26,19 +26,21 @@ function game() {
         threeclubs,
         acespades,
         queenhearts,
+        jackdiamonds,
+        jackdiamonds,
         jackdiamonds
     ];
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     function form(pile) {
-        if(midx-(w*pile.length/2) > .5){
+        if(midx-(w*(pile.length/2)) > 0.5){
             return (midx-(w*pile.length)/2);
         }else{
-            return w/4;
+            return w/3.7;
         }
     }
     function space(pile) {
-        if(midx/pile.length*1.5 <= w) {
-            return midx/pile.length*1.5;
+        if(midx/pile.length*1.7 <= w) {
+            return midx/pile.length*1.7;
         }else{
             return w;
         }
